94f9b984ade3f859e37874fe16fad2c7f03f28db
neskma <87637013+neskma@users.noreply.github.com>
2024-02-23 20:28:14 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func main()
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
+func promptInput() []int {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Введите массив из 6 цифр через пробел:")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	nums := strings.Split(input, " ")
+	arr := make([]int, 0)
+
+	for _, numStr := range nums {
+		num, _ := strconv.Atoi(numStr)
+		arr = append(arr, num)
+	}
+
+	return arr
+}
+
+func main() {
+	arr := promptInput()
+
+	fmt.Println("Исходный массив:", arr)
+
+	bubbleSort(arr)
+
+	fmt.Println("Отсортированный массив:", arr)
+}
