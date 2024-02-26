d4e47e2cecb9fc9c388c5b6fd6194c7a96eb76ea
neskma <87637013+neskma@users.noreply.github.com>
2024-02-27 01:37:53 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func main()
+func main() {
+	// Запрашиваем название файла от пользователя
+	fmt.Print("Введите название файла: ")
+	var fileName string
+	fmt.Scanln(&fileName)
+
+	// Создаем файл только для чтения
+	file, err := os.OpenFile(fileName, os.O_RDONLY|os.O_CREATE|os.O_TRUNC, 0444)
+	if err != nil {
+		fmt.Printf("Ошибка при создании файла: %s\n", err.Error())
+		return
+	}
+	defer file.Close()
+
+	fmt.Printf("Файл %s успешно создан и защищен от записи.\n", fileName)
+}
