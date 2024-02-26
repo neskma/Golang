cd2e6630965f1f6b3d3df1ccfc1df930eb4211ea
neskma <87637013+neskma@users.noreply.github.com>
2024-02-27 01:34:41 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,6 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func main()
+func main() {
+	fileName := "log.txt"
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("Ошибка при открытии файла: %s\n", err.Error())
+		return
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Printf("Ошибка при получении информации о файле: %s\n", err.Error())
+		return
+	}
+
+	if fileInfo.Size() == 0 {
+		fmt.Printf("Файл %s пуст\n", fileName)
+		return
+	}
+
+	var (
+		buffer = make([]byte, fileInfo.Size())
+		offset int64
+	)
+
+	for {
+		n, err := file.ReadAt(buffer, offset)
+		if err != nil {
+			fmt.Printf("Ошибка при чтении файла: %s\n", err.Error())
+			return
+		}
+
+		if n == 0 {
+			break
+		}
+
+		fmt.Print(string(buffer[:n]))
+
+		offset += int64(n)
+	}
+}
