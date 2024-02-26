6e22d96a05068a3ca5004340d28606ec6b92cda1
neskma <87637013+neskma@users.noreply.github.com>
2024-02-27 01:26:31 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"time"
 )
 
-func main()
+func main() {
+	file, err := os.Create("log.txt")
+	if err != nil {
+		fmt.Println("Не удалось создать файл")
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	lineNumber := 1
+
+	for {
+		fmt.Print("Введите строку (для выхода введите 'exit'): ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		if input == "exit" {
+			break
+		}
+
+		timestamp := time.Now().Format("2006-01-02 15:04:05")
+		logMessage := fmt.Sprintf("%d %s %s\n", lineNumber, timestamp, input)
+		fmt.Fprint(file, logMessage)
+
+		lineNumber++
+	}
+
+	fmt.Println("Программа завершена. Лог записан в файл log.txt.")
+}
