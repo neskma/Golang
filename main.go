d75f694fba755a5118a2fe520e660d38b8125c2b
neskma <87637013+neskma@users.noreply.github.com>
2024-02-28 16:34:17 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"time"
 )
 
-func main()
+func main() {
+	done := make(chan struct{})
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Println("Завершение программы...")
+				return
+			default:
+				for i := 1; ; i++ {
+					fmt.Println(i * i)
+					time.Sleep(1 * time.Second)
+				}
+			}
+		}
+	}()
+
+	<-signals
+	close(done)
+	fmt.Println("\nПрограмма завершает работу...")
+}
